15bb9bf5ba7de621cc59f2eccd38dded02ddb9a5
Adrian Stahl <adrianstahl.2@icloud.com>
2023-12-03 13:15:57 +0100

    setup spring-boot application with javafx dependency

---
+++ src/main/java/majestatyczne/bestie/rewardsmanager/RewardsManagerApplication.java
@@ -0,0 +1,13 @@
+package majestatyczne.bestie.rewardsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsManagerApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/majestatyczne/bestie/rewardsmanager/RewardsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package majestatyczne.bestie.rewardsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
