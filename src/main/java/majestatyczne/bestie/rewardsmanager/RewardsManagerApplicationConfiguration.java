3970bd57e2af208ae9fd1d00df29e1487dbd96d7
Wajktor13 <wiktorwilkusz13@gmail.com>
2023-12-04 18:32:36 +0100

    add example.xlsx and change input file path

--- src/main/java/majestatyczne/bestie/rewardsmanager/RewardsManagerApplicationConfiguration.java
+++ src/main/java/majestatyczne/bestie/rewardsmanager/RewardsManagerApplicationConfiguration.java
@@ -10,7 +10,7 @@ public class RewardsManagerApplicationConfiguration {
     @Bean
     CommandLineRunner commandLineRunner(IFileDataLoader dataLoader) {
         return args -> {
-            dataLoader.setInputFilePath("C:\\Users\\wajkt\\Desktop\\projects\\labs\\mi-pt-1300-majestatycznebestie\\src\\main\\resources\\example.xlsx");
+            dataLoader.setInputFilePath("resources/example.xlsx");
             dataLoader.loadData();
         };
     }
