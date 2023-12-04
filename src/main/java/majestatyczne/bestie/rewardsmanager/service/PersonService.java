3b15b0b863bf025f2b532bd49ca2a68324f01938
Wajktor13 <wiktorwilkusz13@gmail.com>
2023-12-04 15:33:53 +0100

    add 'addEntity' methods to all services

--- src/main/java/majestatyczne/bestie/rewardsmanager/service/PersonService.java
+++ src/main/java/majestatyczne/bestie/rewardsmanager/service/PersonService.java
@@ -2,6 +2,7 @@ package majestatyczne.bestie.rewardsmanager.service;
 
 import lombok.AllArgsConstructor;
 import majestatyczne.bestie.rewardsmanager.dao.PersonRepository;
+import majestatyczne.bestie.rewardsmanager.model.Person;
 import org.springframework.stereotype.Service;
 
 
@@ -9,4 +10,8 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class PersonService {
     private final PersonRepository personRepository;
+
+    public void addPerson(Person person) {
+        this.personRepository.save(person);
+    }
 }
--- src/main/java/majestatyczne/bestie/rewardsmanager/service/QuizService.java
+++ src/main/java/majestatyczne/bestie/rewardsmanager/service/QuizService.java
@@ -2,10 +2,15 @@ package majestatyczne.bestie.rewardsmanager.service;
 
 import lombok.AllArgsConstructor;
 import majestatyczne.bestie.rewardsmanager.dao.QuizRepository;
+import majestatyczne.bestie.rewardsmanager.model.Quiz;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class QuizService {
     private final QuizRepository quizRepository;
+
+    public void addQuiz(Quiz quiz) {
+        this.quizRepository.save(quiz);
+    }
 }
