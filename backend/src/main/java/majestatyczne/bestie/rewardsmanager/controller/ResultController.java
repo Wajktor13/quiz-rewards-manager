54c1aaf9d8ad7c461e19ffc899243ec21a916d5e
Wajktor13 <wiktorwilkusz13@gmail.com>
2023-12-19 15:23:06 +0100

    change PathVariable to RequestParam

--- backend/src/main/java/majestatyczne/bestie/rewardsmanager/controller/ResultController.java
+++ backend/src/main/java/majestatyczne/bestie/rewardsmanager/controller/ResultController.java
@@ -2,12 +2,8 @@ package majestatyczne.bestie.rewardsmanager.controller;
 
 import lombok.RequiredArgsConstructor;
 import majestatyczne.bestie.rewardsmanager.dto.ResultDTO;
-import majestatyczne.bestie.rewardsmanager.model.Result;
 import majestatyczne.bestie.rewardsmanager.service.ResultService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,12 +14,13 @@ public class ResultController {
 
     private final ResultService resultService;
 
-    @GetMapping("/by_quiz_id/{quizId}")
-    public List<ResultDTO> getResultsByQuizId(@PathVariable int quizId) {
+    @GetMapping("")
+    public List<ResultDTO> getResultsByQuizId(@RequestParam int quizId) {
         return resultService.findResultsByQuizId(quizId)
                 .stream()
                 .map(result -> new ResultDTO(result.getId(), result.getPerson(), result.getStartDate(),
                         result.getEndDate(), result.getScore()))
                 .toList();
     }
+
 }
