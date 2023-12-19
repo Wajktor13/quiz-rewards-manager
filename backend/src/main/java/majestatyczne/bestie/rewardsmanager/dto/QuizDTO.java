a07f77bcb0c8dffb2c57beec63717dd2802ed96e
Wajktor13 <wiktorwilkusz13@gmail.com>
2023-12-19 14:02:16 +0100

    fix ResultDTO and QuizDTO

--- backend/src/main/java/majestatyczne/bestie/rewardsmanager/dto/QuizDTO.java
+++ backend/src/main/java/majestatyczne/bestie/rewardsmanager/dto/QuizDTO.java
@@ -1,17 +1,29 @@
 package majestatyczne.bestie.rewardsmanager.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class QuizDTO {
 
+    @JsonProperty("quiz_id")
     private int id;
 
+    @JsonProperty("name")
     private String name;
 
+    @JsonProperty("maxScore")
     private int maxScore;
 
+    @JsonProperty("date")
     private Date date;
 }
+
--- backend/src/main/java/majestatyczne/bestie/rewardsmanager/dto/ResultDTO.java
+++ backend/src/main/java/majestatyczne/bestie/rewardsmanager/dto/ResultDTO.java
@@ -1,20 +1,33 @@
 package majestatyczne.bestie.rewardsmanager.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import majestatyczne.bestie.rewardsmanager.model.Person;
 
 import java.util.Date;
 
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class ResultDTO {
 
+    @JsonProperty("result_id")
     private int id;
 
+    @JsonProperty("person_id")
     private Person person;
 
+    @JsonProperty("startDate")
     private Date startDate;
 
+    @JsonProperty("endDate")
     private Date endDate;
 
+    @JsonProperty("score")
     private int score;
 }
+
