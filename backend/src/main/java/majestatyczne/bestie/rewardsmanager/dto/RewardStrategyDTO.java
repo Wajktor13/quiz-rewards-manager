842b4141db04294bcaeea3eaa48fd344a215dc13
Wajktor13 <wiktorwilkusz13@gmail.com>
2024-01-03 18:44:05 +0100

    add annotations RewardStrategyDTO and RewardStrategyParameterDTO

--- backend/src/main/java/majestatyczne/bestie/rewardsmanager/dto/RewardStrategyDTO.java
+++ backend/src/main/java/majestatyczne/bestie/rewardsmanager/dto/RewardStrategyDTO.java
@@ -1,10 +1,16 @@
 package majestatyczne.bestie.rewardsmanager.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import majestatyczne.bestie.rewardsmanager.model.Quiz;
 import majestatyczne.bestie.rewardsmanager.reward_selection_strategy.RewardStrategyType;
 
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class RewardStrategyDTO {
 
     private int id;
--- backend/src/main/java/majestatyczne/bestie/rewardsmanager/dto/RewardStrategyParameterDTO.java
+++ backend/src/main/java/majestatyczne/bestie/rewardsmanager/dto/RewardStrategyParameterDTO.java
@@ -1,5 +1,12 @@
 package majestatyczne.bestie.rewardsmanager.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class RewardStrategyParameterDTO {
 
     private int id;
