a3820b9507d04e00dbb6c04224be7b09b317f34c
Wajktor13 <wiktorwilkusz13@gmail.com>
2024-01-03 20:15:02 +0100

    add RewardStrategyController

---
+++ backend/src/main/java/majestatyczne/bestie/rewardsmanager/controller/RewardStrategyController.java
@@ -0,0 +1,22 @@
+package majestatyczne.bestie.rewardsmanager.controller;
+
+import lombok.RequiredArgsConstructor;
+import majestatyczne.bestie.rewardsmanager.dto.RewardStrategyDTO;
+import majestatyczne.bestie.rewardsmanager.service.RewardStrategyService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("reward-strategy")
+@RequiredArgsConstructor
+public class RewardStrategyController {
+
+    private final RewardStrategyService rewardStrategyService;
+
+    @PostMapping
+    public void addRewardStrategy(@RequestBody RewardStrategyDTO rewardStrategyDTO) {
+        rewardStrategyService.addRewardStrategyFromDTO(rewardStrategyDTO);
+    }
+}
