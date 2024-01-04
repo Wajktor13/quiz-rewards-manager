b87cb2d8b2b883ae565e54c3a94f7884043cfce1
Adrian Stahl <adrianstahl.2@icloud.com>
2024-01-04 19:22:13 +0100

    add percentege strategy, some services return entity not dto

--- backend/src/main/java/majestatyczne/bestie/rewardsmanager/reward_selection_strategy/RewardStrategyType.java
+++ backend/src/main/java/majestatyczne/bestie/rewardsmanager/reward_selection_strategy/RewardStrategyType.java
@@ -2,29 +2,31 @@ package majestatyczne.bestie.rewardsmanager.reward_selection_strategy;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import majestatyczne.bestie.rewardsmanager.model.RewardStrategy;
 
 @Getter
 @AllArgsConstructor
 public enum RewardStrategyType {
 
-    STRATEGY1("X% of the best (fully correct and fastest) - reward of category 1, the rest - reward of" +
+    PERCENTAGE("X% of the best (fully correct and fastest) - reward of category 1, the rest - reward of" +
             " category 2"),
-    STRATEGY2("X correct answers - reward of category 1, Y correct answers - reward of category 2 etc.");
+    SCORE("X correct answers - reward of category 1, Y correct answers - reward of category 2 etc.");
 
     private final String description;
 
-    public RewardSelectionStrategy getRewardSelectionStrategy(majestatyczne.bestie.rewardsmanager.model.RewardStrategy rewardSelection) {
+    public static RewardSelectionStrategy getRewardSelectionStrategy(RewardStrategy rewardSelection) {
         RewardStrategyType rewardSelectionStrategyType = rewardSelection.getRewardStrategyType();
 
         switch (rewardSelectionStrategyType) {
-            case STRATEGY1:
+            case PERCENTAGE:
                 // get parameters for strategy 1...
                 return new PercentageStrategy();
-            case STRATEGY2:
+            case SCORE:
                 // get parameters for strategy 2...
                 return new ScoreStrategy();
             default:
                 throw new IllegalArgumentException("Unsupported strategy type: " + rewardSelectionStrategyType);
         }
     }
+
 }
--- backend/src/main/java/majestatyczne/bestie/rewardsmanager/reward_selection_strategy/ScoreStrategy.java
+++ backend/src/main/java/majestatyczne/bestie/rewardsmanager/reward_selection_strategy/ScoreStrategy.java
@@ -1,4 +1,15 @@
 package majestatyczne.bestie.rewardsmanager.reward_selection_strategy;
 
+import majestatyczne.bestie.rewardsmanager.model.Preference;
+import majestatyczne.bestie.rewardsmanager.model.Quiz;
+import majestatyczne.bestie.rewardsmanager.model.Result;
+import majestatyczne.bestie.rewardsmanager.service.RewardCategoryService;
+
+import java.util.List;
+
 public class ScoreStrategy implements RewardSelectionStrategy {
+    @Override
+    public List<Result> insertRewards(Quiz quiz, List<Preference> preferences) {
+        return null;
+    }
 }
