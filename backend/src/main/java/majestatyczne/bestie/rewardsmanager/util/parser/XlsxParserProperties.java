045db831313b7a3b764f29ec9175b7a3044c01e8
Wajktor13 <wiktorwilkusz13@gmail.com>
2024-01-15 22:30:11 +0100

    adapt parser properties to new file format, change test files

--- backend/src/main/java/majestatyczne/bestie/rewardsmanager/util/parser/XlsxParserProperties.java
+++ backend/src/main/java/majestatyczne/bestie/rewardsmanager/util/parser/XlsxParserProperties.java
@@ -1,12 +1,13 @@
 package majestatyczne.bestie.rewardsmanager.util.parser;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Data
 @Configuration
 @ConfigurationProperties(prefix = "xlsx-parser")
+@Setter
 public class XlsxParserProperties {
 
     private int startDateIndex;
@@ -22,4 +23,39 @@ public class XlsxParserProperties {
     private int notEvaluatedQuestions;
 
     private int columnsPerEvaluatedQuestion;
+
+    @Getter
+    private int emptyTerminalColumns;
+
+    private int applyIndexCorrection(int index) {
+        return index > 0 ? index : index + emptyTerminalColumns;
+    }
+
+    public int getStartDateIndex() {
+        return applyIndexCorrection(startDateIndex);
+    }
+
+    public int getEndDateIndex() {
+        return applyIndexCorrection(endDateIndex);
+    }
+
+    public int getScoreIndex() {
+        return applyIndexCorrection(scoreIndex);
+    }
+
+    public int getPersonNameIndex() {
+        return applyIndexCorrection(personNameIndex);
+    }
+
+    public int getRewardsIndex() {
+        return applyIndexCorrection(rewardsIndex);
+    }
+
+    public int getNotEvaluatedQuestions() {
+        return notEvaluatedQuestions - columnsPerEvaluatedQuestion;
+    }
+
+    public int getColumnsPerEvaluatedQuestion() {
+        return applyIndexCorrection(columnsPerEvaluatedQuestion);
+    }
 }
